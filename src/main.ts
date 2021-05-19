0b97d2498fa7136464fe6181d2c61145b4b147fd
kanga333 <e411z7t40w@gmail.com>
2021-05-19 23:32:02 +0900

    Rename input params

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,28 @@ import * as core from '@actions/core'
 import {buildArray} from './builder'
 import {execCmd} from './execer'
 
-function validate_options(str: string, cmd_stdout: string): void {
-  if ((str === '') === (cmd_stdout === '')) {
-    throw new Error('Need to use only either str or cmd_stdout')
+function validate_options(str: string, cmd: string): void {
+  if ((str === '') === (cmd === '')) {
+    throw new Error('Need to use only either str or cmd')
   }
 }
 
 async function run(): Promise<void> {
   try {
     const str: string = core.getInput('str')
-    const cmd_stdout: string = core.getInput('cmd_stdout')
+    const cmd: string = core.getInput('cmd')
     const separator: string = core.getInput('separator')
-    const append_to: string = core.getInput('append_to')
+    const json_array: string = core.getInput('json_array')
 
-    validate_options(str, cmd_stdout)
+    validate_options(str, cmd)
     let str_to_separate: string
-    if (cmd_stdout !== '') {
-      str_to_separate = await execCmd(cmd_stdout)
+    if (cmd !== '') {
+      str_to_separate = await execCmd(cmd)
     } else {
       str_to_separate = str
     }
 
-    const build: string = buildArray(str_to_separate, separator, append_to)
+    const build: string = buildArray(str_to_separate, separator, json_array)
     core.setOutput('build', build)
   } catch (error) {
     core.error(error)
